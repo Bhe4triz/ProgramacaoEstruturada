e7f060b264c553223dc4a635f81cb6edb444898a
Bhe4triz <100813425+Bhe4triz@users.noreply.github.com>
2023-10-25 11:42:13 -0300

    tem erro ainda na 4

--- 11202131693/lab03/exerc_04.c
+++ 11202131693/lab03/exerc_04.c
@@ -7,8 +7,50 @@ aparece na frase e “nao”, caso contrário. */
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#define MAX 1000
+
+typedef struct epigrafo{
+    char palavra[MAX];
+    char frase[MAX];
+} Texto;
+
+int acha_palavra(char palavra[], char frase[]){
+    int i,j,temPalavra = 0;
+
+    for (i = 0; frase[i] != '\0'; i++){ 
+        for (j = 0; palavra[j] != '\0'; j++){
+            if(palavra[j] == frase[i+j]){
+                temPalavra = 1;     
+            }
+            else{
+                temPalavra = 0;
+                break;
+            }
+        }
+
+         if (temPalavra == 1){
+            break;
+        }
+    }
+    return temPalavra;
+};
 
 int main () {
+    int i;
+    Texto f;
+
+    printf("Digite a palavra e em seguida a frase: \n");
+    fgets(f.palavra, MAX, stdin); 
+    getchar();
+
+    fgets(f.frase, MAX, stdin); //scan do texto
+    
+    if(acha_palavra(f.palavra,f.frase) == 1){
+        printf("sim");
+    }
+    else{
+        printf("nao");
+    }
 
     return 0;
 };
