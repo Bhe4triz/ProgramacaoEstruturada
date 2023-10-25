6b7fea720a3a944db0f35eba4ce5b5ca09d122bd
Bhe4triz <100813425+Bhe4triz@users.noreply.github.com>
2023-10-25 09:51:35 -0300

    .

--- 11202131693/lab03/exerc_03.c
+++ 11202131693/lab03/exerc_03.c
@@ -12,7 +12,28 @@ Saída: sAO pAULO */
 #include <math.h>
 #define MAX 1000
 
+int converte_caracteres(char cadeia[]){
+    int i;
+    //maiscula iniia em 65 ate 90 e minuscula 97 a 122, diferenca de 32
+
+    for (i=0; cadeia[i] != '\0'; i++){
+        if (cadeia[i]>=97 && cadeia[i]<=122)  
+            cadeia[i] = cadeia[i]-32;
+        
+        else if(cadeia[i]>=65 && cadeia[i]<=90)
+            cadeia[i] = cadeia[i]+32;
+    }
+
+    return 0;
+};
+
 int main () {
+    char cadeia[MAX];
+    printf("Digite a cadeia a ser lida: \n");
+    fgets(cadeia, MAX, stdin);
+
+    converte_caracteres(cadeia);
+    printf("%s", cadeia);
 
     return 0;
 };
